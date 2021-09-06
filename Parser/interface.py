3c81159d6de516bf5d203851df5174eae4764671
Yasser Tahiri <yasserth19@gmail.com>
2021-09-06 11:44:00 +0100

    Lint and Fix The Parser

---
+++ Parser/interface.py
@@ -0,0 +1,67 @@
+import uuid
+from Parser.base import JSON_PRC
+from Parser.error import JSONErrors, JSONUnhandledErrorException
+
+
+class DefaultErrorParser:
+    def translate(self, error):
+        code = error["error"]["code"]
+        message = error["error"]["message"]
+        if type(code).__name__ != "int":
+            raise ValueError(
+                "error code is not int by {} in error {}".format(type(code), error)
+            )
+
+        exc = None
+        try:
+            exc = JSONErrors.get(code, message)
+        except KeyError:
+            return JSONUnhandledErrorException(code, message)
+
+
+def json_template(request_id=None):
+    if request_id is None:
+        request_id = str(uuid.uuid4())
+
+    return {
+        "json_prc": JSON_PRC.version_string,
+        "id": request_id,
+        "method": None,
+        "params": [],
+    }
+
+
+def json_request(method, request_id=None):
+    req = json_template(request_id=request_id)
+    req["method"] = method
+    return req
+
+
+def json_result(o, ep):
+    if o.get("error") != None:
+        raise ep.translate(o)
+    return o["result"]
+
+
+def jsonrpc_response(request_id, result):
+    return {
+        "json_prc": JSON_PRC.version_string,
+        "id": request_id,
+        "result": result,
+    }
+
+
+def json_error(request_id, code, message=None):
+    e = JSONErrors.get(code, message)
+    return {
+        "json_prc": JSON_PRC.version_string,
+        "id": request_id,
+        "error": {
+            "code": code,
+            "message": str(e),
+        },
+    }
+
+
+def json_is_response_to(request, response):
+    return request["id"] == response["id"]
